89f7ae71386b1b36f4f8667558934281f8452139
Felix Anducho <felix.anducho74@gmail.com>
2023-08-31 21:20:06 -0600

    Added new cmd flag to count bytes and update tests

--- chapter-01/main.go
+++ chapter-01/main.go
@@ -10,15 +10,20 @@ import (
 
 func main() {
 	lines := flag.Bool("l", false, "Count lines")
+	totalBytes := flag.Bool("b", false, "Count bytes")
 
 	flag.Parse()
 
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *totalBytes))
 }
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines bool, countBytes bool) int {
 	//A scanner is used to read text from a Reader(such as files)
 	scanner := bufio.NewScanner(r)
+
+	if countBytes {
+		scanner.Split(bufio.ScanBytes)
+	}
 
 	if !countLines {
 		scanner.Split(bufio.ScanWords)
