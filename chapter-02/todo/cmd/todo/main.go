5f70fb5eb929dab95e931b03ff9a58a40f89670c
Felix Anducho <felix.anducho74@gmail.com>
2023-09-08 21:00:41 -0600

    Implement the delete flag

--- chapter-02/todo/cmd/todo/main.go
+++ chapter-02/todo/cmd/todo/main.go
@@ -17,6 +17,7 @@ func main() {
 	add := flag.Bool("add", false, "Add task to the ToDo list")
 	list := flag.Bool("list", false, "List all tasks")
 	complete := flag.Int("complete", 0, "Item to be completed")
+	delete := flag.Int("del", 0, "Item to be deleted")
 
 	flag.Parse()
 
@@ -52,6 +53,16 @@ func main() {
 			os.Exit(1)
 		}
 		l.Add(task)
+
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	case *delete > 0:
+		if err := l.Delete(*delete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
