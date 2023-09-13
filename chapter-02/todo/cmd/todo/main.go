1b5005859d0452d677f2fe5ff97e42ab29903dd6
Felix Anducho <felix.anducho74@gmail.com>
2023-09-12 22:46:24 -0600

    Update custom usage function. Add additional instructions on adding new tasks

--- chapter-02/todo/cmd/todo/main.go
+++ chapter-02/todo/cmd/todo/main.go
@@ -21,6 +21,15 @@ func main() {
 	verbose := flag.Bool("verbose", false, "Show information like date/time")
 	notComplete := flag.Bool("not-complete", false, "List all the not completed tasks")
 
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(),
+			"%s tool. Developed by Felix Anducho\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "Copyright 2023\n")
+		fmt.Fprintln(flag.CommandLine.Output(), "Usage information:")
+		fmt.Fprintln(flag.CommandLine.Output(), "Add new task adding the -add flag to the execution of the program. Example: ./todo -add this is a new task")
+		flag.PrintDefaults()
+	}
+
 	flag.Parse()
 
 	if os.Getenv("TODO_FILENAME") != "" {
