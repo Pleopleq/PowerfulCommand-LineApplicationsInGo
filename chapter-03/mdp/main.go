ba93edbe34c2256b69fdad298ec1ca544129492a
Felix Anducho <felix.anducho74@gmail.com>
2023-09-18 21:00:28 -0600

    Move gitignore to root. Start chapter 03 initial files

---
+++ chapter-03/mdp/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+const (
+	header = `
+	<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+`
+
+	footer = `
+	</body>
+</html>
+`
+)
+
+func main() {
+	filename := flag.String("file", "", "Markdown file to preview")
+	flag.Parse()
+
+	if *filename == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if err := run(*filename); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
